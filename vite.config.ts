cd542fac627a62b778affb66f8b9d873e1950578
丘惟夫 <waveciou@gmail.com>
2022-03-08 22:40:52 +0800

    config setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const assetsDir = 'resources';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    assetsDir: assetsDir,
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        entryFileNames: `${assetsDir}/js/main.js`,
+        chunkFileNames: `${assetsDir}/js/[name].js`,
+        assetFileNames: ({ name = '' }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name)){
+            return `${assetsDir}/img/[name].[ext]`;
+          }
+          if (/\.css$/.test(name)) {
+            return `${assetsDir}/css/[name].[ext]`;
+          }
+          return `${assetsDir}/[name].[ext]`;
+        }
+      }
+    },
+  }
 });
