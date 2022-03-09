ae4c768126ec9c03f21014b3ecce7b423071bf10
丘惟夫 <waveciou@gmail.com>
2022-03-09 23:35:14 +0800

    config setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,31 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import react from '@vitejs/plugin-react';
 
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 const assetsDir = 'resources';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root,
+  plugins: [
+    react(),
+    // createHtmlPlugin({
+    //   minify: true,
+    // })
+  ],
   build: {
-    assetsDir: assetsDir,
+    outDir,
+    assetsDir,
     assetsInlineLimit: 0,
+    emptyOutDir: true,
     rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about', 'index.html')
+      },
       output: {
         entryFileNames: `${assetsDir}/js/main.js`,
         chunkFileNames: `${assetsDir}/js/[name].js`,
